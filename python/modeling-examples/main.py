19a95b9108b69bd581e11f447801022addb838ec
Alessandro Ricci <a.ricci@unibo.it>
2024-04-23 16:42:30 +0200

    Smart Room example extended

--- python/modeling-examples/main.py
+++ python/modeling-examples/main.py
@@ -1,22 +1,50 @@
 # from mix import MyCounter, User
-from smart_room import Light
+from smart_room import Light, LightSwitch, SmartRoom
 
 if __name__ == '__main__':
     '''
-    counter = MyCounter(10)
-    counter.inc()
-    counter.inc()
-    v = counter.get_value()
-    print("Counter value: ")
-    print(str(v))
+    # creating  a light 
 
-    user = User("Paula","secret","User-001")
-    print(user.get_pwd())
-    user.updatePassword("secret2")
-    print(user.get_pwd())
+    l = Light("myLight")
+
+    if l.isOn():
+        print("The light is on.")
+    else: 
+        print("The light is off.")
+
+    # turning on the light
+
+    l.turnOn()
+
+    # creating a light switch connected to the light
+
+    s = LightSwitch("mySwitch",l)
+
+    # pressing the light switch
+
+    s.press()
+
+    print(l.isOn())
+
+    # pressing again
+
+    s.press()
+
+    print(l.isOn())
     '''
+    room = SmartRoom("myRoom")
 
-    light = Light("myLight")
+    newLight = Light("light-03")
+    room.addNewLight(newLight)
 
+    room.press("switch-01")
+    
+    room.turnOn("light-02")
 
+    try: 
+        room.turnOn("light-66")
+    except:
+        print("some error occurred.")
+
+    room.lock()
 
